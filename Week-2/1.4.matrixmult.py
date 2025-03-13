44f421a7dcd5f0adbdb792674d2388a9883f3d5f
Jeongsik Park <ori@khu.ac.kr>
2025-03-13 15:26:50 +0900

    week2

--- Week-2/1.4.matrixmult.py
+++ Week-2/1.4.matrixmult.py
@@ -1,10 +1,13 @@
-# name: 
-# student id: 
+# name: 박정식
+# student id: 2023105656
 from typing import List
 
 def matrixmult(n: int, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
     C = [[0] * n for _ in range(n)]
 
-    # Complete the code here
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                C[i][k] += A[i][j] * B[j][k]
 
     return C
