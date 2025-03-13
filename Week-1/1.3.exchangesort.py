1911fcaa0badcb6c78877ebb7e44d4f6b2f5c28a
Jeongsik Park <ori@khu.ac.kr>
2025-03-13 15:27:41 +0900

    week1 1.3 edited

--- Week-1/1.3.exchangesort.py
+++ Week-1/1.3.exchangesort.py
@@ -2,7 +2,12 @@ from typing import List
 
 def exchangesort(n: int, S: List[int]) -> None:
     # Complete the code here
-    for i in range(0, n-1):
-        for j in range(0, n-i-1):
-            if S[j] > S[j+1]:
-                S[j], S[j+1] = S[j+1], S[j]
+    # for i in range(0, n-1):
+    #     for j in range(0, n-i-1):
+    #         if S[j] > S[j+1]:
+    #             S[j], S[j+1] = S[j+1], S[j]
+
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if S[i] > S[j]:
+                S[i], S[j] = S[j], S[i]
