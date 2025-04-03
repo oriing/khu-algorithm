60469a9f2e5e8a9c58785a3232d2bebec869f96f
Jeongsik Park <ori@khu.ac.kr>
2025-04-03 15:21:17 +0900

    Week5

--- Week-5/3.1.bin.py
+++ Week-5/3.1.bin.py
@@ -4,4 +4,4 @@ def bin(n: int, k: int) -> int:
     if k == 0 or n == k:
         return 1
     else:
-        # Complete the code here
+        return bin(n-1, k-1) + bin(n-1, k)
--- Week-5/3.4.floyd2.py
+++ Week-5/3.4.floyd2.py
@@ -3,13 +3,20 @@ from typing import List
 def floyd2(n: int, W: List[List[int]]) -> List[List[int]]:
     P = [[-1] * (n) for _ in range(n)]
     D = W
-    # Complete the code here
     
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                v = D[i][k] + D[k][j]
+                if v < D[i][j]:
+                    D[i][j] = v
+                    P[i][j] = k
+
     return D, P
 
 def path(i: int, j: int, P: list[list[int]]):
     k = P[i][j]
     if k != -1:
-        path(i, k)
+        path(i, k, P)
         print("v" + str(k), end = " ")
-        path(k, j)
+        path(k, j, P)
