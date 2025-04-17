70aec1e135b0c2454ec982565a1cf1d8e207fd34
Jeongsik Park <ori@khu.ac.kr>
2025-04-17 15:56:31 +0900

    week7

--- Week-7/4.1.prim.py
+++ Week-7/4.1.prim.py
@@ -2,14 +2,26 @@ from typing import List, Tuple
 
 def prim(n: int, W: List[List[float]]) -> List[Tuple[int, int, float]]:
     F = []
-    nearest = [-1] * (n + 1)
+    nearest  = [-1] * (n + 1)
     distance = [-1] * (n + 1)
     INF = float("inf")
     for i in range(2, n + 1):
-        nearest[i] = 1
+        nearest[i]  = 1
         distance[i] = W[1][i]
     for _ in range(n - 1):
         min = INF
-        # Complete the code here
+        for i in range(2, n+1):
+            if distance[i] == -1: continue
+            if min > distance[i]:
+                min  = distance[i]
+                edge = (nearest[i], i, distance[i])
+
+        F.append(edge)
+        distance[edge[1]] = -1
+        
+        for i in range(2, n+1):
+            if distance[i] > W[edge[1]][i]:
+                nearest[i]  = edge[1]
+                distance[i] = W[edge[1]][i]
 
     return F
